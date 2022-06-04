2a6deb205808add4939efaab432ceeb2c97b43a2
Aleksandr Molodykh <caneyok@gmail.com>
2022-06-04 13:45:45 +0300

    Модель получает данные с сервера и выводит их в консоль

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import FilmsPresenter from './presenter/film-board-presenter.js';
 import FilmCardModel from './model/film-card-model.js';
 import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
+import FilmCardsApiService from './film-cards-api-service.js';
 import { render } from './framework/render.js';
+
+const AUTHORIZATION = 'Basic fsdf221q3llpf099';
+const END_POINT = 'https://17.ecmascript.pages.academy/cinemaddict/';
 
 const siteMain = document.querySelector('.main');
 
-const filmCardModel = new FilmCardModel();
+const filmCardModel = new FilmCardModel(new FilmCardsApiService(END_POINT, AUTHORIZATION));
+
 const filterModel = new FilterModel();
 
 const filmContainer = new FilmsSectionView();
--- src/model/film-card-model.js
+++ src/model/film-card-model.js
@@ -4,6 +4,14 @@ import { generateFilm } from '../mock/film-data.js';
 
 export default class FilmCardModel extends Observable{
   #filmCards = Array.from({length: 14}, generateFilm);
+  #filmCardsApiService = null;
+
+  constructor(filmCardsApiService) {
+    super();
+    this.#filmCardsApiService = filmCardsApiService;
+
+    this.#filmCardsApiService.filmCards.then((filmCards) => console.log(filmCards.map(this.#adaptToClient)));
+  }
 
   get filmCards() {
     return this.#filmCards;
@@ -39,6 +47,39 @@ export default class FilmCardModel extends Observable{
     ];
 
     this._notify(updateType, update);
+  };
+
+  #adaptToClient = (filmCard) => {
+    const adaptedFilmCard = {
+      ...filmCard,
+      filmInfo: filmCard['film_info'],
+      userDetails: filmCard['user_details'],
+    };
+
+    delete adaptedFilmCard['film_info'];
+    delete adaptedFilmCard['user_details'];
+
+    adaptedFilmCard.filmInfo = {
+      ...adaptedFilmCard.filmInfo,
+      originalTitle: adaptedFilmCard.filmInfo['alternative_title'],
+      totalRating: adaptedFilmCard.filmInfo['total_rating'],
+      ageRating: adaptedFilmCard.filmInfo['age_rating'],
+    };
+
+    delete adaptedFilmCard.filmInfo['alternative_title'];
+    delete adaptedFilmCard.filmInfo['total_rating'];
+    delete adaptedFilmCard.filmInfo['age_rating'];
+
+    adaptedFilmCard.userDetails = {
+      ...adaptedFilmCard.userDetails,
+      watched: adaptedFilmCard.userDetails['alredy_watched'],
+      watchingDate: adaptedFilmCard.userDetails['watching_date'],
+    };
+
+    delete adaptedFilmCard.userDetails['alredy_watched'];
+    delete adaptedFilmCard.userDetails['watching_date'];
+
+    return adaptedFilmCard;
   };
 }
 
